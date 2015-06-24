a8dd13aac3c3bae4531fdc31ba08ab10c2235a8a
Muusha <bicheuxj@gmail.com>
2015-06-24 18:30:01 +0200

    DB

--- public/app.js
+++ public/app.js
@@ -2,19 +2,12 @@ var http = require('http');
 var express = require('express');
 var Client = require('../library/client');
 
+//Access DB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk("localhost:27017/db");
+
 var app = express();
-
-var client1 = new Client('fozeek');
-
-var client2 = new Client('muusha');
-// client2.getStorage().mkdir('lolilol');
-// client2.getStorage().mkdir('lolilol/1');
-// client2.getStorage().rmdir('lolilol/1');
-client2.getStorage().write('lolilol/2/monfichier.txt', 'COUCOUCOUCOUC');
-// client2.getStorage().write('lolilol/2/monfichier2.txt', 'COUCOUCOUCOUC33333');
-// client2.getStorage().write('lolilol/2/monfichier3.txt', 'COUCOUCOUCOUC5555555');
-// client2.getStorage().remove('lolilol/2/monfichier3.txt');
-client1.getStorage().write('mon/path/monfichier.txt', 'ANNNH');
 
 app.set('views', 'templates');
 app.set('view engine', 'ejs');
@@ -26,8 +19,25 @@ app.set('view engine', 'ejs');
 //Acces aux objets statiques
 //app.use(express.static(path.join(__dirname, 'views')));
 
+//var collection = db.get("User");
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.get('/', function(req, res){
     res.render('index.ejs');
+});
+
+app.get('/test', function(req, res){
+    var db = req.db;
+    var collection = db.get('User');
+    collection.find({},{},function(e,docs){
+        res.render('test.ejs', {
+            "userlist" : docs
+        });
+    });
 });
 
 var server = app.listen(3000);
