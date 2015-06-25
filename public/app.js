7844ec03d8d070cb355d164bc95b9236d1dd1fe7
Muusha <bicheuxj@gmail.com>
2015-06-25 09:35:04 +0200

    Update

--- public/app.js
+++ public/app.js
@@ -3,27 +3,23 @@ var express = require('express');
 var Client = require('../library/client');
 var Storage = require('../library/storage');
 var fs = require('graceful-fs');
-
+var parser = require('body-parser');
 
 //Access DB
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk("localhost:27017/db");
 
-var app = express();
-
-app.set('views', 'templates');
-
-app.set('view engine', 'ejs');
-
-//var parser = require('bodyParser');
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
-
 //Acces aux objets statiques
 //app.use(express.static(path.join(__dirname, 'views')));
 
-//var collection = db.get("User");
+var app = express();
+app.use(parser.urlencoded({extended:true})); 
+app.use(parser.json());
+//app.use(express.bodyParser());
+
+app.set('views', 'templates');
+app.set('view engine', 'ejs');
 
 app.use(function(req,res,next){
     req.db = db;
@@ -37,10 +33,16 @@ app.get('/', function(req, res){
 app.post('/account', function(req, res){
     var db = req.db;
     var collection = db.get('User');
+
+    //pseudo:req.body.pseudo, password:req.body.password
+    console.log(collection);
+
     collection.find({},{},function(e,docs){
-        res.render('test.ejs', {
-            "userlist" : docs
-        });
+        //res.render('test.ejs', {
+          //  "userlist" : docs
+        //});
+        console.log(e, docs);
+        res.send(docs);
     });
 });
 
@@ -50,16 +52,19 @@ app.get('/nodes', function(req, res){
         res.render('account', {user: user, files: files, path:req.originalUrl});
     });
 });
+
 app.get('/nodes/:hash', function(req, res){
     var storage = new Storage(req.params.hash);
     storage.list(function(files) {
         res.render('list', {storage: storage, files: files, path:req.originalUrl});
     });
 });
+
 app.get('/nodes/:hash/blob/*', function(req, res){
     blob = req.params[0];
     res.render('file', {file: blob});
 });
+
 app.get('/nodes/:hash/download/*', function(req, res){
     var storage = new Storage(req.params.hash);
     storage.download(req.params[0], function(file, tmp){
